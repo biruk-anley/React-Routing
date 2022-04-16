72f3637b104acde6254858c36070fac0951eba6d
biruk <birukanley2022@gmail.com>
2022-04-16 13:36:01 +0300

    alter the path

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ import Navbar from './Navbar';
 import Home from './Home';
 import Create from './Create';
 import Contact from './Contact';
+import Null from './Null';
+import { useState } from 'react/cjs/react.development';
 
 function App() {
+  {/* Aleter the boolean as true and false to checkout conditional rendering */}
+  const[cardisempty]=useState(true)
   return (
     <Router>
       <div className="App">
@@ -14,8 +18,14 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/create" element={<Create />} />
-            <Route path="/contact" element={<Contact />} />
+            <Route path="/contact/*" element={<Contact />} />   {/*  Nested route :: */}
             <Route path="/redirect" element={<Navigate to="/create" />} />
+
+            {/* this is to chckout condtional rendering */}
+            <Route
+              path='/check'
+              element ={cardisempty? <Navigate to= "/contact"/>:<Null/>}
+            />
           </Routes>
         </div>
       </div>
