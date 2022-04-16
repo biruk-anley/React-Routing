c99d52d87f00aea829e2cad40fa69b2ee3a026bf
biruk <birukanley2022@gmail.com>
2022-04-16 13:02:51 +0300

    Add nested route

---
+++ src/Nestedroute.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Nestedroute = () => {
+    return (
+        <div>
+            <h2>I am nested route ok</h2>
+            <p>Lorem ipsum dolor sit amet consectetur,
+                adipisicing elit. Repudiandae libero illo por'
+                ro eos tempore voluptatibus velit facere eius veritatis reprehenderit error, quibusdam, suscipit fugit maiores magnam,
+                magni ut. Sunt illum ten
+                etur corporis.</p>
+            <h2>Admit me</h2>
+            <h4>Adddd me</h4>
+        </div>
+    );
+};
+
+export default Nestedroute;
