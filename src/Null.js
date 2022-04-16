0626455f26e6452e4f1095b235b6aef3dcfeb286
biruk <birukanley2022@gmail.com>
2022-04-16 13:35:20 +0300

    Adding nested route

---
+++ src/Null.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Null = () => {
+    return (
+        <div>
+            <h2>
+                this is the page that come out because of page is empty
+            </h2>
+            <p>emptytytytytytyty</p>
+        </div>
+    );
+};
+
+export default Null;
