75f22c2be468e9cc0e362acd78b58ca0d3224d32
biruk <birukanley2022@gmail.com>
2022-04-16 13:03:31 +0300

    create page

---
+++ src/Create.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Create = () => {
+    return (
+        <div>
+            <h2>Creating Page</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A ipsum illo autem temporibus dolorem accusantium obcaecati similique corporis?</p>
+        </div>
+    );
+};
+
+export default Create;
